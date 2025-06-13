2a7c10e2c107ee14c54a2c2c7b9e08acdf82d521
Dineth0 <dinethnakandala10@gmail.com>
2025-06-13 10:26:14 +0530

    update connection pool

--- src/main/java/lk/ijse/gdse/ConnectionPool.java
+++ src/main/java/lk/ijse/gdse/ConnectionPool.java
@@ -1,5 +1,36 @@
 package lk.ijse.gdse;
 
-public class ConnectionPool
-{
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.SQLException;
+@WebListener
+public class ConnectionPool implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        BasicDataSource basicDataSource = new BasicDataSource();
+
+        basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        basicDataSource.setUrl("jdbc:mysql://localhost:3306/CMS");
+        basicDataSource.setUsername("root");
+        basicDataSource.setPassword("Ijse@1234");
+        basicDataSource.setInitialSize(5);
+        basicDataSource.setMaxTotal(5);
+
+        ServletContext servletContext = sce.getServletContext();
+        servletContext.setAttribute("dataSource", basicDataSource);
+
+    }
+    public void contextDestroyed(ServletContextEvent sce) {
+        try{
+            ServletContext servletContext = sce.getServletContext();
+            BasicDataSource basicDataSource = (BasicDataSource) servletContext.getAttribute("dataSource");
+            basicDataSource.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
